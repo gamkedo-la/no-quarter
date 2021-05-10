ae02bfb4abe2dbfc4dc6b08f41d2789c9da0ed95
IanCher <IanCher@users.noreply.github.com>
2021-05-10 22:41:33 +0200

    WIP Enemy Charger AI - Enemy goes for the players only if they are within a certain distance

--- Assets/Scripts/EnemyChargerAI.cs
+++ Assets/Scripts/EnemyChargerAI.cs
@@ -6,18 +6,19 @@ using UnityEngine.AI;
 public class EnemyChargerAI : MonoBehaviour
 {
     [SerializeField] Transform player = null;
+    [SerializeField] float detectionDistance = 20.0f;
     [SerializeField] bool debug = false;
 
     private LineRenderer lineRenderer = null;
 
-
     private NavMeshAgent navMeshAgent;
+
+    private bool isPlayerLocated = false;
 
     // Start is called before the first frame update
     void Start()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
-        navMeshAgent.destination = player.position;
 
         if (debug){
             lineRenderer = gameObject.AddComponent(typeof(LineRenderer)) as LineRenderer;
@@ -27,12 +28,36 @@ public class EnemyChargerAI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        LookForPlayer();
+        ChasePlayerIfLocated();
+
         ShowPathIfDebugMode();
+    }
+
+    private void LookForPlayer()
+    {
+        float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+
+        if (distanceToPlayer < detectionDistance)
+        {
+            isPlayerLocated = true;
+        }
+        else
+        {
+            isPlayerLocated = false;
+        }
+    }
+
+    private void ChasePlayerIfLocated()
+    {
+        if (!isPlayerLocated) { return; }
+
+        navMeshAgent.destination = player.position;
     }
 
     private void ShowPathIfDebugMode()
     {
-        if (!debug || !navMeshAgent.hasPath) {return;}
+        if (!debug || !navMeshAgent.hasPath) { return; }
 
         lineRenderer.positionCount = navMeshAgent.path.corners.Length;
         lineRenderer.SetPositions(navMeshAgent.path.corners);
