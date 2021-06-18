adf55ea365579f82f22836114c9828de265b1628
IanCher <IanCher@users.noreply.github.com>
2021-06-18 21:20:39 +0200

    Copied part of the code from FPSWeapon in range enemy to make it shoot 1 bullet straight ahead - no really aiming at the player yet

--- Assets/Scripts/EnemyRangeAI.cs
+++ Assets/Scripts/EnemyRangeAI.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +7,7 @@ using UnityEngine.AI;
 public class EnemyRangeAI : MonoBehaviour
 {
     [SerializeField] Projectile projectile;
+    [SerializeField] Transform firePosition = null;
 
     private GameObject player;
     private NavMeshAgent navMeshAgent;
@@ -15,11 +17,61 @@ public class EnemyRangeAI : MonoBehaviour
     {
         player = GameObject.FindGameObjectWithTag("Player");
         navMeshAgent = GetComponent<NavMeshAgent>();
+
+        StartCoroutine(Think());
+    }
+    private IEnumerator Think()
+    {
+        while (true) {
+            Fire(transform.forward);
+            yield return new WaitForSeconds(UnityEngine.Random.Range(1.0f, 2.5f));
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         navMeshAgent.SetDestination(player.transform.position);
+    }
+
+    private void Fire(Vector3 fireDirection)
+    {
+        var fireDirections = GetProjectileDirections(fireDirection);
+
+        foreach (var dir in fireDirections)
+        {
+            var projectileInstance = Instantiate(projectile, firePosition.position, player.transform.rotation);
+            var projectileComponent = projectileInstance.GetComponent<Projectile>();
+            projectileComponent.projectileDirection = dir;
+            // projectileComponent.originator = this;
+        }
+    }
+
+    List<Vector3> GetProjectileDirections(Vector3 originalDirection)
+    {
+        var numProjectiles = GetProjectileCount();
+        List<Vector3> directions = new List<Vector3>();
+
+        if (numProjectiles > 1)
+        {
+            for (var i = 0; i < numProjectiles; i++)
+            {
+                Vector3 spread = UnityEngine.Random.insideUnitCircle * 0.1f;
+                var skewedDirection = originalDirection + (spread.x * player.transform.right) + (spread.y * player.transform.up);
+                directions.Add(skewedDirection);
+            }
+        }
+        else
+        {
+            directions.Add(originalDirection);
+        }
+
+        return directions;
+    }
+
+    int GetProjectileCount()
+    {
+        var projectileCount = projectile.baseProjectileCount;
+        return projectileCount;
     }
 }
