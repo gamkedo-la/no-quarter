c9b33d396d1fc086ad723318721f75e48e97c098
Tyler Funk <tcfunk25@gmail.com>
2021-07-15 11:23:17 -0500

    theme loading screen

--- Assets/LoadingText.cs
+++ Assets/LoadingText.cs
@@ -1,10 +1,4 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
 using TMPro;
-using Unity.Mathematics;
 using UnityEngine;
 
 public class LoadingText : MonoBehaviour
@@ -15,22 +9,11 @@ public class LoadingText : MonoBehaviour
     public float timeBetweenDotChange = 0.333f;
     private string[] loadingMessages;
     private int currentMessage = 0;
-
-    private PlayerInputHandler[] players;
+    private float timeSinceChange;
 
     // Start is called before the first frame update
     void Start()
     {
-        players = FindObjectsOfType<PlayerInputHandler>();
-
-        if (players.Length > 0)
-        {
-            foreach (var player in players)
-            {
-                player.enabled = false;
-            }
-        }
-
         loadingMessages = new string[maxDots+1];
         loadingMessages[0] = baseText;
         for (var i = 1; i < loadingMessages.Length; i++)
@@ -38,27 +21,16 @@ public class LoadingText : MonoBehaviour
             loadingMessages[i] = baseText + new string('.', i);
         }
 
-        StartCoroutine(UpdateLoadingText());
     }
 
-    // private void OnDestroy()
-    // {
-    //     if (players.Length > 0)
-    //     {
-    //         foreach (var player in players)
-    //         {
-    //             if (player != null) player.enabled = true;
-    //         }
-    //     }
-    // }
-
-    IEnumerator UpdateLoadingText()
+    private void Update()
     {
-        while (true)
+        timeSinceChange += Time.deltaTime;
+        if (timeSinceChange >= timeBetweenDotChange)
         {
-            yield return new WaitForSeconds(timeBetweenDotChange);
             currentMessage = (currentMessage + 1) % (loadingMessages.Length);
             loadingTextObject.text = loadingMessages[currentMessage];
+            timeSinceChange = 0f;
         }
     }
 }
